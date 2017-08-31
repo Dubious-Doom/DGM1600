1c5fab15aa5c01b86ace6ee4717df8a58851f797
Dubious_Doom <drew.fischer11@gmail.com>
2017-08-31 11:16:06 -0600

    Did some work on script "Guessing"

--- Guessinator/Assets/Guessing.cs
+++ Guessinator/Assets/Guessing.cs
@@ -6,7 +6,15 @@ public class Guessing : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		print ("Welcome to Number Guesser");
+		print ("Pick a number in your head");
+
+		print ("The highest number you can pick is 100");
+		print ("The lowest number you can pick is 1");
+
+		print ("Is the number higher or lower than 50?");
+		print ("Up arrow for higher, Down for lower, Enter for equal");
+
 	}
 	
 	// Update is called once per frame
