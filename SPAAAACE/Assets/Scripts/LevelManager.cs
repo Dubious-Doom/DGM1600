88debe9332c4875272ab8bba4c6158c3086dd974
Dubious_Doom <drew.fischer11@gmail.com>
2017-11-01 14:41:37 -0600

    Made two new levels for Brick Breaker; started work on Space Game

---
+++ SPAAAACE/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	void Start(){
+		
+	}
+		
+		
+	public void LevelLoad(string lvl){
+		SceneManager.LoadScene (lvl);
+	}
+
+	public void ExitGame (){ //Not have this function with a mobile app
+		print ("Tried to exit.");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+		
+	}
+
---
+++ SPAAAACE/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rigid;
+	private Transform transform;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody2D> ();
+		transform = this.GetComponent<Transform> ();
+		speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			print ("Up");
+			rigid.velocity = new Vector2 (0, speed);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			print ("Down");
+			rigid.velocity = new Vector2 (0, -speed);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			print ("Right");
+			rigid.velocity = new Vector2 (speed, 0);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			print ("Left");
+			rigid.velocity = new Vector2 (-speed, 0);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.RightArrow)) {
+			print ("Up and Right");
+			rigid.velocity = new Vector2 (speed, speed);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.RightArrow)) {
+			print ("Down and Right");
+			rigid.velocity = new Vector2 (speed, -speed);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.LeftArrow)) {
+			print ("Down and Left");
+			rigid.velocity = new Vector2 (-speed, -speed);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.LeftArrow)) {
+			print ("Up and Left");
+			rigid.velocity = new Vector2 (-speed, speed);
+		}
+	}
+}
