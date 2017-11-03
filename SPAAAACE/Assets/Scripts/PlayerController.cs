0b250a7927265fe0e2f849b3f9c58803d240fb1e
Dubious_Doom <drew.fischer11@gmail.com>
2017-11-02 20:52:12 -0600

    Added meteors to space game

--- SPAAAACE/Assets/Scripts/PlayerController.cs
+++ SPAAAACE/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	//Specify a variable for the 
 	private Rigidbody2D rigid;
 	private Transform transform;
 	public float speed;
@@ -20,41 +21,49 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			print ("Up");
 			rigid.velocity = new Vector2 (0, speed);
+			transform.localEulerAngles = new Vector3 (0, 0, 0);
 		}
 
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			print ("Down");
 			rigid.velocity = new Vector2 (0, -speed);
-		}
+			transform.localEulerAngles = new Vector3 (0, 0, 180);
+				}
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			print ("Right");
 			rigid.velocity = new Vector2 (speed, 0);
+			transform.localEulerAngles = new Vector3 (0, 0, -90);
 		}
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			print ("Left");
 			rigid.velocity = new Vector2 (-speed, 0);
+			transform.localEulerAngles = new Vector3 (0, 0, 90);
 		}
 
 		if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.RightArrow)) {
 			print ("Up and Right");
 			rigid.velocity = new Vector2 (speed, speed);
+			transform.localEulerAngles = new Vector3 (0, 0, -45);
 		}
 
 		if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.RightArrow)) {
 			print ("Down and Right");
 			rigid.velocity = new Vector2 (speed, -speed);
+			transform.localEulerAngles = new Vector3 (0, 0, -135);
 		}
 
 		if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.LeftArrow)) {
 			print ("Down and Left");
 			rigid.velocity = new Vector2 (-speed, -speed);
+			transform.localEulerAngles = new Vector3 (0, 0, 135);
 		}
 
 		if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.LeftArrow)) {
 			print ("Up and Left");
 			rigid.velocity = new Vector2 (-speed, speed);
+			transform.localEulerAngles = new Vector3 (0, 0, 45);
 		}
 	}
 }
